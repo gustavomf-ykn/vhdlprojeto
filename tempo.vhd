937a4e9f091af58949a2de4e6df12f30ec271933
gustavomf-ykn <gustavomf.empresa@gmail.com>
2026-07-02 18:29:27 -0300

    Atualiza tempo

--- tempo.vhd
+++ tempo.vhd
@@ -1,1 +1,32 @@
-hello
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity tempo_mod is
+port(R: in std_logic; clock: in std_logic; E: in std_logic; tempo: out std_logic_vector(3 downto 0); fim_tempo: out std_logic);
+end tempo_mod;
+
+architecture bhv of tempo_mod is
+    signal count: std_logic_vector(3 downto 0) := "0000";
+    signal done: std_logic := '0';
+begin
+    process(clock, R)
+    begin
+        if R = '1' then
+            count <= "0000";
+            done <= '0';
+        elsif rising_edge(clock) then
+            if E = '1' then
+                if done = '0' then
+                    if count = "1001" then
+                        done <= '1';
+                    else
+                        count <= count + 1;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
+    tempo <= count;
+    fim_tempo <= done;
+end bhv;
